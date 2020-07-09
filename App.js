34965ae1c02c65f2688c0db5297ec63a0c88a4b1
Thetamine <sruss0911@gmail.com>
2020-07-09 09:25:45 -0500

    section 3 complete

--- App.js
+++ App.js
@@ -3,16 +3,18 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from './src/screens/ComponentsScreen';
 import Ex1Screen from './src/screens/Ex1Screen';
+import ListScreen from './src/screens/ListScreen';
 
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
-    Ex1: Ex1Screen
+    Ex1: Ex1Screen,
+    List: ListScreen
   },
   {
-    initialRouteName: "Ex1",
+    initialRouteName: "Home",
     defaultNavigationOptions: {
       title: "App"
     }
